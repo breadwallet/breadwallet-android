354695abfa06066e2c82517df419f84f89173775
Gutyn <Mishulika92@gmail.com>
2015-08-18 14:47:16 -0700

    Lunch commit

--- app/src/androidTest/java/com/breadwallet/presenter/currencyformatting/tests/CurrencyFormatterFixedTests.java
+++ app/src/androidTest/java/com/breadwallet/presenter/currencyformatting/tests/CurrencyFormatterFixedTests.java
@@ -1,23 +1,21 @@
-package com.breadwallet.presenter.activities.tests;
-
-import android.test.ActivityInstrumentationTestCase2;
+package com.breadwallet.presenter.currencyformatting.tests;
 
 /**
  * BreadWallet
- *
- * Created by Mihail on 7/30/15.
+ * <p/>
+ * Created by Mihail on 8/18/15.
  * Copyright (c) 2015 Mihail Gutan <mihail@breadwallet.com>
- *
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,10 +24,5 @@ import android.test.ActivityInstrumentationTestCase2;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
-public class ScanResultActivityTest extends ActivityInstrumentationTestCase2 {
-
-    public ScanResultActivityTest() {
-        super(ScanResultActivity.class);
-    }
+public class CurrencyFormatterFixedTests {
 }
