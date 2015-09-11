cee903f5276b9b096721ef199f5d0ab1f4f5341d
Gutyn <Mishulika92@gmail.com>
2015-09-10 21:57:59 -0700

    fixed the bug created by the bug fixes that I thougth would fix a bug

--- app/src/main/java/com/breadwallet/presenter/fragments/MainFragmentSettingsAll.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/MainFragmentSettingsAll.java
@@ -15,20 +15,20 @@ import com.breadwallet.tools.animation.FragmentAnimator;
 
 /**
  * BreadWallet
- *
+ * <p/>
  * Created by Mihail on 8/4/15.
  * Copyright (c) 2015 Mihail Gutan <mihail@breadwallet.com>
- *
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,7 +44,7 @@ public class MainFragmentSettingsAll extends Fragment {
     private RelativeLayout settings;
     private RelativeLayout importPrivateKeys;
     private MainActivity app;
-    private MainFragmentSettingsAll mainMainFragmentSettingsAll;
+    private MainFragmentSettingsAll mainFragmentSettingsAll;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -53,7 +53,8 @@ public class MainFragmentSettingsAll extends Fragment {
         // properly.
         final View rootView = inflater.inflate(
                 R.layout.fragment_settings_all, container, false);
-        importPrivateKeys = (RelativeLayout) rootView.findViewById(R.id.importprivatekey);
+        app = MainActivity.app;
+        importPrivateKeys = (RelativeLayout) rootView.findViewById(R.id.import_private_key);
         importPrivateKeys.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,25 +63,24 @@ public class MainFragmentSettingsAll extends Fragment {
                 }
             }
         });
-
+        mainFragmentSettingsAll = this;
+        settings = (RelativeLayout) rootView.findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (FragmentAnimator.checkTheMultipressingAvailability(300)) {
+                    FragmentAnimator.animateSlideToLeft(app, app.fragmentSettings, mainFragmentSettingsAll);
+                    Log.d(TAG, "Starting:   showBouncySlide()");
+                }
+            }
+        });
         return rootView;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        app = MainActivity.app;
-        mainMainFragmentSettingsAll = this;
-        settings = (RelativeLayout) getView().findViewById(R.id.settings);
-        settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (FragmentAnimator.checkTheMultipressingAvailability(300)) {
-                    FragmentAnimator.animateSlideToLeft(app, app.fragmentSettings, mainMainFragmentSettingsAll);
-                    Log.d(TAG, "Starting:   showBouncySlide()");
-                }
-            }
-        });
+
     }
 
     @Override
