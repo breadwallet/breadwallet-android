4cd8bda126f647fb408594bd95c3e1bd9e80d3e0
Gutyn <Mishulika92@gmail.com>
2015-10-06 00:19:21 -0700

    some changes

--- app/src/main/java/com/breadwallet/presenter/fragments/FragmentWipeWallet.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/FragmentWipeWallet.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.breadwallet.R;
 import com.breadwallet.presenter.BreadWalletApp;
@@ -62,6 +65,14 @@ public class FragmentWipeWallet extends Fragment {
         recoveryPhraseEditText = (EditText) rootView.findViewById(R.id.editText_phrase);
         wipe = (Button) rootView.findViewById(R.id.wipe_wallet_wipe);
         recoveryPhraseEditText.setText("");
+        recoveryPhraseEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    wipe.performClick();
+                }
+                return false;
+            }
+        });
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
