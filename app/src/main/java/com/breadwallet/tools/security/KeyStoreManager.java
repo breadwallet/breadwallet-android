787cbafa81eadcaa0d6a9d072aff2b8b8ca8f8b1
Gutyn <Mishulika92@gmail.com>
2016-04-21 18:56:48 -0700

    mainnet and took off debugging stuff

--- app/src/main/java/com/breadwallet/tools/security/KeyStoreManager.java
+++ app/src/main/java/com/breadwallet/tools/security/KeyStoreManager.java
@@ -94,7 +94,7 @@ public class KeyStoreManager {
     private static final String WALLET_CREATION_TIME_FILENAME = "my_creation_time";
     private static final String PASS_CODE_FILENAME = "my_pass_code";
 
-    private static final int AUTH_DURATION_SEC = 5;//TODO make 300
+    private static final int AUTH_DURATION_SEC = 300;//TODO make 300
 //    private static final int CANARY_AUTH_DURATION_SEC = Integer.MAX_VALUE;
 
     public static boolean putKeyStorePhrase(String strToStore, Activity context, int requestCode) {
