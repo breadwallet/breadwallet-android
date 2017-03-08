7694a474be03b137e283c308b045fb23b99672ec
Gutyn <Mishulika92@gmail.com>
2017-03-08 09:45:17 -0800

    add sepparate shape xml file for the second button

--- app/src/main/java/com/breadwallet/presenter/activities/IntroRecoverWordsActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/IntroRecoverWordsActivity.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 
 import com.breadwallet.R;
 import com.breadwallet.presenter.customviews.BRSoftKeyboard;
+import com.breadwallet.tools.animation.SpringAnimator;
 
 import static com.breadwallet.R.color.dark_blue;
 import static com.breadwallet.R.color.light_gray;
@@ -59,18 +60,19 @@ public class IntroRecoverWordsActivity extends Activity {
         GradientDrawable leftDrawable = (GradientDrawable) leftButton.getBackground().getCurrent();
         GradientDrawable rightDrawable = (GradientDrawable) rightButton.getBackground().getCurrent();
         int rad = 30;
+        int stoke = 3;
 
         leftDrawable.setCornerRadii(new float[]{rad, rad, 0, 0, 0, 0, rad, rad});
         rightDrawable.setCornerRadii(new float[]{0, 0, rad, rad, rad, rad, 0, 0});
 
         if (isLeft) {
-            leftDrawable.setStroke(2, getColor(dark_blue), 0, 0);
-            rightDrawable.setStroke(2, getColor(light_gray), 0, 0);
+            leftDrawable.setStroke(stoke, getColor(dark_blue), 0, 0);
+            rightDrawable.setStroke(stoke, getColor(light_gray), 0, 0);
             leftButton.setTextColor(getColor(dark_blue));
             rightButton.setTextColor(getColor(light_gray));
         } else {
-            leftDrawable.setStroke(2, getColor(light_gray), 0, 0);
-            rightDrawable.setStroke(2, getColor(dark_blue), 0, 0);
+            leftDrawable.setStroke(stoke, getColor(light_gray), 0, 0);
+            rightDrawable.setStroke(stoke, getColor(dark_blue), 0, 0);
             leftButton.setTextColor(getColor(light_gray));
             rightButton.setTextColor(getColor(dark_blue));
         }
