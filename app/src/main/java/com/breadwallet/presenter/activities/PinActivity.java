68db495e452c1c653ff2eb36243c969f7b934942
Gutyn <Mishulika92@gmail.com>
2017-03-10 14:24:25 -0800

    lock screen with awesome animations on unlock

--- app/src/main/java/com/breadwallet/presenter/activities/PinActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/PinActivity.java
@@ -1,5 +1,7 @@
 package com.breadwallet.presenter.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
@@ -9,14 +11,21 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.breadwallet.R;
 import com.breadwallet.presenter.customviews.BRSoftKeyboard;
 import com.breadwallet.tools.animation.SpringAnimator;
 import com.breadwallet.tools.security.KeyStoreManager;
 
+import static android.R.attr.duration;
 import static com.breadwallet.R.color.dark_blue;
 import static com.breadwallet.R.color.extra_light_grey;
 import static com.breadwallet.R.color.white;
@@ -34,6 +43,11 @@ public class PinActivity extends Activity {
     private StringBuilder pin = new StringBuilder();
     private int pinLimit = 6;
 
+    private ImageView unlockedImage;
+    private TextView unlockedText;
+    private TextView enterPinLabel;
+    private LinearLayout offlineButtonsLayout;
+
     private Button leftButton;
     private Button rightButton;
 
@@ -44,6 +58,11 @@ public class PinActivity extends Activity {
         setStatusBarColor(android.R.color.transparent);
         keyboard = (BRSoftKeyboard) findViewById(R.id.brkeyboard);
         pinLayout = (LinearLayout) findViewById(R.id.pinLayout);
+
+        unlockedImage = (ImageView) findViewById(R.id.unlocked_image);
+        unlockedText = (TextView) findViewById(R.id.unlocked_text);
+        enterPinLabel = (TextView) findViewById(R.id.enter_pin_label);
+        offlineButtonsLayout = (LinearLayout) findViewById(R.id.buttons_layout);
 
         dot1 = findViewById(R.id.dot1);
         dot2 = findViewById(R.id.dot2);
@@ -164,18 +183,9 @@ public class PinActivity extends Activity {
                 public void run() {
                     String actualPin = KeyStoreManager.getPassCode(PinActivity.this);
                     if (actualPin.equalsIgnoreCase(pin.toString())) {
-                        Intent intent = new Intent(PinActivity.this, BreadActivity.class);
-                        startActivity(intent);
-                        pin = new StringBuilder("");
+                        unlockWallet();
                     } else {
-                        SpringAnimator.failShakeAnimation(PinActivity.this, pinLayout);
-                        pin = new StringBuilder("");
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                updateDots();
-                            }
-                        },1000);
+                        showFailedToUnlock();
                     }
 
                 }
@@ -183,6 +193,103 @@ public class PinActivity extends Activity {
 
         }
 
+    }
+
+    private void unlockWallet() {
+        pin = new StringBuilder("");
+        int duration = 2000;
+//        TranslateAnimation translateUp = new TranslateAnimation(offlineButtonsLayout.getX(), offlineButtonsLayout.getX(), offlineButtonsLayout.getY(), -200);
+//        translateUp.setDuration(duration);
+//        translateUp.setInterpolator(new AccelerateInterpolator());
+//        setAnimationListenerToView(offlineButtonsLayout, translateUp);
+//        offlineButtonsLayout.startAnimation(translateUp);
+        offlineButtonsLayout.animate().translationY(-600).setInterpolator(new AccelerateInterpolator());
+        pinLayout.animate().translationY(-2000).setInterpolator(new AccelerateInterpolator());
+        enterPinLabel.animate().translationY(-1800).setInterpolator(new AccelerateInterpolator());
+        keyboard.animate().translationY(600).setInterpolator(new AccelerateInterpolator());
+        unlockedImage.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                Intent intent = new Intent(PinActivity.this, BreadActivity.class);
+                startActivity(intent);
+            }
+        });
+        unlockedText.animate().alpha(1f);
+
+//        TranslateAnimation translateUp2 = new TranslateAnimation(pinLayout.getX(), pinLayout.getX(), pinLayout.getY(), -200);
+//        translateUp2.setDuration(duration);
+//        translateUp2.setInterpolator(new AccelerateInterpolator());
+//        setAnimationListenerToView(pinLayout, translateUp2);
+//        pinLayout.startAnimation(translateUp2);
+//
+//        TranslateAnimation translateUp3 = new TranslateAnimation(enterPinLabel.getX(), enterPinLabel.getX(), enterPinLabel.getY(), -200);
+//        translateUp3.setDuration(duration);
+//        translateUp3.setInterpolator(new AccelerateInterpolator());
+//        setAnimationListenerToView(enterPinLabel, translateUp3);
+//        enterPinLabel.startAnimation(translateUp3);
+//
+//        TranslateAnimation translateDown = new TranslateAnimation(keyboard.getX(), keyboard.getX(), keyboard.getY(), 2000);
+//        translateDown.setDuration(duration);
+//        translateDown.setInterpolator(new AccelerateInterpolator());
+//        setAnimationListenerToView(keyboard, translateDown);
+//        keyboard.startAnimation(translateDown);
+//
+//        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+//        alphaAnimation.setDuration(duration);
+//        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//                Intent intent = new Intent(PinActivity.this, BreadActivity.class);
+//                startActivity(intent);
+//                unlockedImage.setAlpha(1f);
+//                unlockedText.setAlpha(1f);
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//
+//            }
+//        });
+//        unlockedText.startAnimation(alphaAnimation);
+//        unlockedImage.startAnimation(alphaAnimation);
+
+
+    }
+
+    private void setAnimationListenerToView(final View v, Animation animation) {
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                v.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void showFailedToUnlock() {
+        SpringAnimator.failShakeAnimation(PinActivity.this, pinLayout);
+        pin = new StringBuilder("");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                updateDots();
+            }
+        }, 1000);
     }
 
     private void chooseWordsSize(boolean isLeft) {
