624d5c23d2a10e031ee6cc725fe3dcda4f6e12b6
Gutyn <Mishulika92@gmail.com>
2017-04-27 18:03:34 -0700

    add seekBar to the spending limit

--- app/src/main/java/com/breadwallet/presenter/activities/settings/SpendLimitActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/settings/SpendLimitActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.breadwallet.R;
 import com.breadwallet.presenter.activities.ActivityUTILS;
@@ -13,11 +15,14 @@ import com.breadwallet.tools.animation.BreadDialog;
 import com.breadwallet.tools.manager.SharedPreferencesManager;
 import com.breadwallet.wallet.BRPeerManager;
 
+import static com.breadwallet.R.id.textView;
+
 public class SpendLimitActivity extends AppCompatActivity {
     private static final String TAG = SpendLimitActivity.class.getName();
-    private Button scanButton;
+    //    private Button scanButton;
     public static boolean appVisible = false;
     private static SpendLimitActivity app;
+    private SeekBar seekBar;
 
     public static SpendLimitActivity getApp() {
         return app;
@@ -28,33 +33,26 @@ public class SpendLimitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spend_limit);
         ActivityUTILS.setStatusBarColor(this, R.color.status_bar);
-        scanButton = (Button) findViewById(R.id.button_scan);
-        scanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!BRAnimator.isClickAllowed()) return;
-                BreadDialog.showCustomDialog(SpendLimitActivity.this, "Sync with Blockchain?",
-                        "You will not be able to send money while syncing.", "Sync", "Cancel",
-                        new BRDialogView.BROnClickListener() {
-                            @Override
-                            public void onClick(BRDialogView brDialogView) {
-                                brDialogView.dismissWithAnimation();
-                                new Thread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        BRPeerManager.getInstance().rescan();
-                                        SharedPreferencesManager.putStartHeight(SpendLimitActivity.this, BRPeerManager.getCurrentBlockHeight());
-                                        BRAnimator.startBreadActivity(SpendLimitActivity.this, false);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(200);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
 
-                                    }
-                                }).start();
-                            }
-                        }, new BRDialogView.BROnClickListener() {
-                            @Override
-                            public void onClick(BRDialogView brDialogView) {
-                                brDialogView.dismissWithAnimation();
-                            }
-                        }, null, 0);
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+//                textView.setText("Covered: " + progress + "/" + seekBar.getMax());
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
             }
         });
 
