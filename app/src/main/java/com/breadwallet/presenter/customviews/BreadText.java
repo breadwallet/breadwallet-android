94d3c55ceeb45a964e463d9b0929d252ceeeffd6
Gutyn <Mishulika92@gmail.com>
2017-05-03 12:37:36 -0700

    Extend TextView class and create BreadText class in order to override and have the ability to input custom fonts in xml files

---
+++ app/src/main/java/com/breadwallet/presenter/customviews/BreadText.java
@@ -0,0 +1,81 @@
+package com.breadwallet.presenter.customviews;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.breadwallet.R;
+
+/**
+ * BreadWallet
+ * <p/>
+ * Created by Mihail Gutan on <mihail@breadwallet.com> 5/3/17.
+ * Copyright (c) 2017 breadwallet LLC
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+@SuppressLint("AppCompatCustomView") // we don't need to support older versions
+public class BreadText extends TextView {
+    private static final String TAG = BreadText.class.getName();
+
+    public BreadText(Context context) {
+        super(context);
+    }
+
+    public BreadText(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public BreadText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    public BreadText(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.BreadText);
+        String customFont = a.getString(R.styleable.BreadText_customFont);
+        setCustomFont(ctx, "fonts/" + customFont);
+        a.recycle();
+    }
+
+    public boolean setCustomFont(Context ctx, String asset) {
+        Typeface tf = null;
+        try {
+            tf = Typeface.createFromAsset(ctx.getAssets(), asset);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not get typeface: " + e.getMessage());
+            return false;
+        }
+
+        setTypeface(tf);
+        return true;
+    }
+
+}
