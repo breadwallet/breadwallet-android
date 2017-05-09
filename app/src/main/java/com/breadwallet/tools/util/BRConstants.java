2dcff47d7a93f7e2d2df73abdeb20304abf57e0a
Gutyn <Mishulika92@gmail.com>
2017-05-08 17:07:28 -0700

    turn off the platform

--- app/src/main/java/com/breadwallet/tools/util/BRConstants.java
+++ app/src/main/java/com/breadwallet/tools/util/BRConstants.java
@@ -179,7 +179,7 @@ public class BRConstants {
 
     public static final long PASS_CODE_TIME_LIMIT = TimeUnit.MILLISECONDS.convert(6, TimeUnit.DAYS);
 
-    public static final boolean PLATFORM_ON = true;
+    public static final boolean PLATFORM_ON = false;
 
     private BRConstants() {
     }
