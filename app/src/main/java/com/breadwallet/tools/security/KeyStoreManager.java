7520d36fdcc495998c16efed083b9021b4d787ce
Gutyn <Mishulika92@gmail.com>
2017-06-02 13:16:02 -0700

    assertion for intent

--- app/src/main/java/com/breadwallet/tools/security/KeyStoreManager.java
+++ app/src/main/java/com/breadwallet/tools/security/KeyStoreManager.java
@@ -648,6 +648,7 @@ public class KeyStoreManager {
         // we will provide a generic one for you if you leave it null
         KeyguardManager mKeyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
         Intent intent = mKeyguardManager.createConfirmDeviceCredentialIntent(context.getString(R.string.auth_required), context.getString(R.string.auth_message));
+        Assert.assertTrue(intent != null);
         if (intent != null) {
             context.startActivityForResult(intent, requestCode);
         } else {
