f1f6e6d5df5f87b7611a9a679f74ae0f34c28328
Gutyn <Mishulika92@gmail.com>
2017-06-07 18:54:21 -0700

    fix start paper key

--- app/src/main/java/com/breadwallet/presenter/activities/intro/WriteDownActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/intro/WriteDownActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.breadwallet.R;
 import com.breadwallet.presenter.activities.util.ActivityUTILS;
@@ -16,6 +17,7 @@ import com.breadwallet.tools.util.BRConstants;
 public class WriteDownActivity extends BRActivity {
     private static final String TAG = WriteDownActivity.class.getName();
     private Button writeButton;
+    private ImageButton close;
     public static boolean appVisible = false;
     private static WriteDownActivity app;
 
@@ -29,6 +31,13 @@ public class WriteDownActivity extends BRActivity {
         setContentView(R.layout.activity_write_down);
 
         writeButton = (Button) findViewById(R.id.button_write_down);
+        close = (ImageButton) findViewById(R.id.close_button);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                close();
+            }
+        });
         writeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,15 +64,14 @@ public class WriteDownActivity extends BRActivity {
 
     @Override
     public void onBackPressed() {
-        int count = getFragmentManager().getBackStackEntryCount();
-        if (count == 0) {
-            BRAnimator.startBreadActivity(this, false);
-            if (!isDestroyed()) finish();
-            //additional code
-        } else {
-            getFragmentManager().popBackStack();
-        }
+        super.onBackPressed();
 
+    }
+
+    private void close() {
+        BRAnimator.startBreadActivity(this, false);
+        if (!isDestroyed()) finish();
+        //additional code
     }
 
     @Override
