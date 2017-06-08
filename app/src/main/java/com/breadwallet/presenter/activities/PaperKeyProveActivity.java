d1f4523dfb372dd77f82ca3c3488bc2c5ed6b3d1
Gutyn <Mishulika92@gmail.com>
2017-06-08 12:11:59 -0700

    fix confirm paper key screen

--- app/src/main/java/com/breadwallet/presenter/activities/PaperKeyProveActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/PaperKeyProveActivity.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.transition.TransitionManager;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,6 +46,9 @@ public class PaperKeyProveActivity extends BRActivity {
     private SparseArray<String> sparseArrayWords = new SparseArray<>();
     public static boolean appVisible = false;
     private static PaperKeyProveActivity app;
+    private ConstraintLayout constraintLayout;
+    private ConstraintSet applyConstraintSet = new ConstraintSet();
+    private ConstraintSet resetConstraintSet = new ConstraintSet();
 
     public static PaperKeyProveActivity getApp() {
         return app;
@@ -58,14 +66,57 @@ public class PaperKeyProveActivity extends BRActivity {
         wordTextFirst = (TextView) findViewById(R.id.word_number_first);
         wordTextSecond = (TextView) findViewById(R.id.word_number_second);
 
-        new Handler().postDelayed(new Runnable() {
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        resetConstraintSet.clone(constraintLayout);
+        applyConstraintSet.clone(constraintLayout);
+
+        wordEditFirst.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void run() {
-                wordEditFirst.requestFocus();
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+            public void onFocusChange(View v, boolean hasFocus) {
+                TransitionManager.beginDelayedTransition(constraintLayout);
+                if (hasFocus) {
+                    applyConstraintSet.setMargin(R.id.word_number_first, ConstraintSet.TOP, 8);
+                    applyConstraintSet.setMargin(R.id.word_edittext_first, ConstraintSet.TOP, 16);
+                    applyConstraintSet.applyTo(constraintLayout);
+                } else {
+                    resetConstraintSet.applyTo(constraintLayout);
+                }
             }
-        }, 200);
+        });
+
+        wordEditSecond.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+//                TransitionManager.beginDelayedTransition(constraintLayout);
+                if (hasFocus) {
+                    applyConstraintSet.setMargin(R.id.word_number_second, ConstraintSet.TOP, 8);
+                    applyConstraintSet.setMargin(R.id.word_edittext_second, ConstraintSet.TOP, 16);
+                    applyConstraintSet.applyTo(constraintLayout);
+                } else {
+                    resetConstraintSet.applyTo(constraintLayout);
+                }
+            }
+        });
+
+        wordEditSecond.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.submit || id == EditorInfo.IME_NULL) {
+                    submit.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                wordEditFirst.requestFocus();
+//                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+//            }
+//        }, 200);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,12 +126,14 @@ public class PaperKeyProveActivity extends BRActivity {
                 String edit2 = wordEditSecond.getText().toString().replaceAll("[^a-zA-Z]", "");
 
                 if (edit1.equalsIgnoreCase(sparseArrayWords.get(sparseArrayWords.keyAt(0))) && edit2.equalsIgnoreCase(sparseArrayWords.get(sparseArrayWords.keyAt(1)))) {
+                    Utils.hideKeyboard(PaperKeyProveActivity.this);
                     SharedPreferencesManager.putPhraseWroteDown(PaperKeyProveActivity.this, true);
                     BRAnimator.showBreadSignal(PaperKeyProveActivity.this, "Paper Key Set", "Awesome!", R.drawable.ic_check_mark_white, new BROnSignalCompletion() {
                         @Override
                         public void onComplete() {
                             BRAnimator.startBreadActivity(PaperKeyProveActivity.this, false);
                             overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left);
+                            finishAffinity();
                         }
                     });
                 } else {
@@ -93,11 +146,11 @@ public class PaperKeyProveActivity extends BRActivity {
                         throw new NullPointerException("No word list");
                     }
                     Log.e(TAG, "onClick: FAIL");
-                    if (!list.contains(edit1) || edit1.equalsIgnoreCase(sparseArrayWords.get(sparseArrayWords.keyAt(0)))) {
+                    if (!list.contains(edit1) || !edit1.equalsIgnoreCase(sparseArrayWords.get(sparseArrayWords.keyAt(0)))) {
                         SpringAnimator.failShakeAnimation(PaperKeyProveActivity.this, wordTextFirst);
                     }
 
-                    if (!list.contains(edit2) || edit2.equalsIgnoreCase(sparseArrayWords.get(sparseArrayWords.keyAt(1)))) {
+                    if (!list.contains(edit2) || !edit2.equalsIgnoreCase(sparseArrayWords.get(sparseArrayWords.keyAt(1)))) {
                         SpringAnimator.failShakeAnimation(PaperKeyProveActivity.this, wordTextSecond);
                     }
                 }
