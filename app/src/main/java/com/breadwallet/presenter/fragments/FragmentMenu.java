45917fc10a9c5487f75a33b88c64e18b848b246d
Gutyn <Mishulika92@gmail.com>
2017-06-08 17:28:52 -0700

    finally match menu screen

--- app/src/main/java/com/breadwallet/presenter/fragments/FragmentMenu.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/FragmentMenu.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -68,7 +69,7 @@ public class FragmentMenu extends Fragment {
     public ListView mListView;
     public RelativeLayout background;
     public List<BRMenuItem> itemList;
-    public LinearLayout signalLayout;
+    public ConstraintLayout signalLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -78,7 +79,7 @@ public class FragmentMenu extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
         background = (RelativeLayout) rootView.findViewById(R.id.layout);
-        signalLayout = (LinearLayout) rootView.findViewById(R.id.signal_layout);
+        signalLayout = (ConstraintLayout) rootView.findViewById(R.id.signal_layout);
         background.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
