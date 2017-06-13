7942125ac98bb724b1e31423a5b46af777269a0d
Gutyn <Mishulika92@gmail.com>
2017-06-13 16:19:15 -0700

    refactor all the button selectors for the new BRButton

--- app/src/main/java/com/breadwallet/presenter/customviews/BRButton.java
+++ app/src/main/java/com/breadwallet/presenter/customviews/BRButton.java
@@ -2,13 +2,7 @@ package com.breadwallet.presenter.customviews;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -50,9 +44,9 @@ import com.breadwallet.tools.util.Utils;
 @SuppressLint("AppCompatCustomView") // we don't need to support older versions
 public class BRButton extends Button {
     private static final String TAG = BRButton.class.getName();
-    private final int ANIMATION_DURATION = 200;
-    private int currentX = 0;
-    private int currentY = 0;
+    private final int ANIMATION_DURATION = 50;
+    //    private int currentX = 0;
+//    private int currentY = 0;
     private boolean isBreadButton; //meaning is has the special animation and shadow
 
     public BRButton(Context context) {
@@ -96,40 +90,47 @@ public class BRButton extends Button {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-//        if (isBreadButton) {
-//            Log.e(TAG, "onTouchEvent: touched!" );
-//
-//            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//                if (getParent() != null) {
-//                    getParent().requestDisallowInterceptTouchEvent(true);
-//                }
-//
-//                ScaleAnimation scaleAnim = new ScaleAnimation(
-//                        1f, 0.8f,
-//                        1f, 0.8f,
-//                        Animation.RELATIVE_TO_SELF, 1,
-//                        Animation.RELATIVE_TO_SELF, 1);
-//                scaleAnim.setDuration(ANIMATION_DURATION);
-//                scaleAnim.setRepeatCount(0);
-//                scaleAnim.setInterpolator(new AccelerateDecelerateInterpolator());
-//                scaleAnim.setFillAfter(true);
-//                scaleAnim.setFillBefore(true);
-//                scaleAnim.setFillEnabled(true);
-//
-//                startAnimation(scaleAnim);
-////        else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-////            if (clicked_on_image) {
-////                //do stuff, drag the image or whatever
-////            }
-////        }
-//                return super.onTouchEvent(event);
-//            } else if (event.getAction() == MotionEvent.ACTION_UP) {
-////            clicked_on_image = false;
-//            }
-//            return true;
-//        } else {
-        return super.onTouchEvent(event);
-//        }
+        if (isBreadButton) {
+
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                if (getParent() != null) {
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                }
+
+                ScaleAnimation scaleAnim = new ScaleAnimation(
+                        1f, 0.9f,
+                        1f, 0.9f,
+                        Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 1f);
+                scaleAnim.setDuration(ANIMATION_DURATION);
+                scaleAnim.setRepeatCount(0);
+                scaleAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+                scaleAnim.setFillAfter(true);
+                scaleAnim.setFillBefore(true);
+                scaleAnim.setFillEnabled(true);
+
+                startAnimation(scaleAnim);
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//            clicked_on_image = false;
+                ScaleAnimation scaleAnim = new ScaleAnimation(
+                        0.9f, 1f,
+                        0.9f, 1f,
+                        Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 1f);
+                scaleAnim.setDuration(ANIMATION_DURATION);
+                scaleAnim.setRepeatCount(0);
+                scaleAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+                scaleAnim.setFillAfter(true);
+                scaleAnim.setFillBefore(true);
+                scaleAnim.setFillEnabled(true);
+
+                startAnimation(scaleAnim);
+
+            }
+            return super.onTouchEvent(event);
+        } else {
+            return super.onTouchEvent(event);
+        }
 
     }
 
