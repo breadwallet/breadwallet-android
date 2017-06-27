25db3b3950fb736e456b7284fe7dff18f7879b24
Gutyn <Mishulika92@gmail.com>
2017-06-27 14:17:18 -0700

    create custom notification bar

---
+++ app/src/main/java/com/breadwallet/presenter/customviews/BRNotificationBar.java
@@ -0,0 +1,237 @@
+package com.breadwallet.presenter.customviews;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Handler;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.breadwallet.R;
+import com.breadwallet.presenter.activities.BreadActivity;
+
+/**
+ * BreadWallet
+ * <p/>
+ * Created by Mihail Gutan on <mihail@breadwallet.com> 5/8/17.
+ * Copyright (c) 2017 breadwallet LLC
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+public class BRNotificationBar extends android.support.v7.widget.Toolbar {
+
+    private static final String TAG = BRNotificationBar.class.getName();
+
+    private EditText searchEdit;
+    //    private LinearLayout filterButtonsLayout;
+    private BRButton sentFilter;
+    private BRButton receivedFilter;
+    private BRButton pendingFilter;
+    private BRButton completedFilter;
+    private BRButton cancelButton;
+    private BreadActivity breadActivity;
+    private BRText description;
+
+    public boolean[] filterSwitches = new boolean[4];
+
+    public BRNotificationBar(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public BRNotificationBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public BRNotificationBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        inflate(getContext(), R.layout.notification_bar, this);
+        breadActivity = (BreadActivity) getContext();
+        description = (BRText) findViewById(R.id.description);
+
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.BRLinearLayoutWithCaret);
+        final int N = a.getIndexCount();
+        for (int i = 0; i < N; ++i) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.BRNotificationBar_breadText:
+                    String text = a.getString(0);
+                    description.setText(text);
+                    break;
+            }
+        }
+        a.recycle();
+//        searchEdit = (EditText) findViewById(R.id.search_edit);
+//        sentFilter = (BRButton) findViewById(R.id.sent_filter);
+//        receivedFilter = (BRButton) findViewById(R.id.received_filter);
+//        pendingFilter = (BRButton) findViewById(R.id.pending_filter);
+//        completedFilter = (BRButton) findViewById(R.id.complete_filter);
+//        cancelButton = (BRButton) findViewById(R.id.cancel_button);
+
+//        clearSwitches();
+//
+//        setListeners();
+//
+//        searchEdit.requestFocus();
+//        searchEdit.postDelayed(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                InputMethodManager keyboard = (InputMethodManager)
+//                        getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+//                keyboard.showSoftInput(searchEdit, 0);
+//            }
+//        }, 200); //use 300 to make it run when coming back from lock screen
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                breadActivity.updateTxList();
+//            }
+//        }).start();
+
+    }
+
+    private void updateFilterButtonsUI(boolean[] switches) {
+        sentFilter.setBackgroundResource(switches[0] ? R.drawable.selector_blue_stroke : R.drawable.selector_gray_stroke);
+        sentFilter.setTextColor(switches[0] ? getContext().getColor(R.color.dark_blue) : getContext().getColor(R.color.light_gray));
+        receivedFilter.setBackgroundResource(switches[1] ? R.drawable.selector_blue_stroke : R.drawable.selector_gray_stroke);
+        receivedFilter.setTextColor(switches[1] ? getContext().getColor(R.color.dark_blue) : getContext().getColor(R.color.light_gray));
+        pendingFilter.setBackgroundResource(switches[2] ? R.drawable.selector_blue_stroke : R.drawable.selector_gray_stroke);
+        pendingFilter.setTextColor(switches[2] ? getContext().getColor(R.color.dark_blue) : getContext().getColor(R.color.light_gray));
+        completedFilter.setBackgroundResource(switches[3] ? R.drawable.selector_blue_stroke : R.drawable.selector_gray_stroke);
+        completedFilter.setTextColor(switches[3] ? getContext().getColor(R.color.dark_blue) : getContext().getColor(R.color.light_gray));
+        if (breadActivity.adapter != null)
+            breadActivity.adapter.filterBy(searchEdit.getText().toString(), filterSwitches);
+    }
+
+    private void setListeners() {
+        searchEdit.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (breadActivity.barFlipper != null) {
+                        breadActivity.barFlipper.setDisplayedChild(0);
+                        clearSwitches();
+                    }
+                }
+            }
+        });
+
+        cancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                breadActivity.barFlipper.setDisplayedChild(0);
+                clearSwitches();
+                onShow(false);
+            }
+        });
+
+        searchEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (breadActivity.adapter != null)
+                    breadActivity.adapter.filterBy(s.toString(), filterSwitches);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        sentFilter.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filterSwitches[0] = !filterSwitches[0];
+                filterSwitches[1] = false;
+                updateFilterButtonsUI(filterSwitches);
+
+            }
+        });
+
+        receivedFilter.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filterSwitches[1] = !filterSwitches[1];
+                filterSwitches[0] = false;
+                updateFilterButtonsUI(filterSwitches);
+            }
+        });
+
+        pendingFilter.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filterSwitches[2] = !filterSwitches[2];
+                filterSwitches[3] = false;
+                updateFilterButtonsUI(filterSwitches);
+            }
+        });
+
+        completedFilter.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filterSwitches[3] = !filterSwitches[3];
+                filterSwitches[2] = false;
+                updateFilterButtonsUI(filterSwitches);
+            }
+        });
+    }
+
+    public void clearSwitches() {
+        filterSwitches[0] = false;
+        filterSwitches[1] = false;
+        filterSwitches[2] = false;
+        filterSwitches[3] = false;
+    }
+
+    public void onShow(boolean b) {
+        final InputMethodManager keyboard = (InputMethodManager)
+                getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (b) {
+            clearSwitches();
+            updateFilterButtonsUI(filterSwitches);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    searchEdit.requestFocus();
+                    keyboard.showSoftInput(searchEdit, 0);
+                }
+            }, 400);
+        } else {
+            keyboard.hideSoftInputFromWindow(searchEdit.getWindowToken(), 0);
+        }
+    }
+
+}
