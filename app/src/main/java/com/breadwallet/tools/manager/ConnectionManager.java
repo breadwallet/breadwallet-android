300fe92e8d6a4753ee7b0e2a54305fb2ffd213fa
Gutyn <Mishulika92@gmail.com>
2017-06-27 19:24:57 -0700

    create a new ConnectionManager and us new JobScheduler api

---
+++ app/src/main/java/com/breadwallet/tools/manager/ConnectionManager.java
@@ -0,0 +1,100 @@
+package com.breadwallet.tools.manager;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+
+import com.breadwallet.BreadWalletApp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * BreadWallet
+ * <p/>
+ * Created by Mihail Gutan <mihail@breadwallet.com> on 7/14/15.
+ * Copyright (c) 2016 breadwallet LLC
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+public class ConnectionManager{
+
+    private static final String TAG = ConnectionManager.class.getName();
+
+    private static ConnectionManager instance;
+    public static final int MY_BACKGROUND_JOB = 0;
+
+    private ConnectionManager() {
+        connectionReceiverListeners = new ArrayList<>();
+        startNetworkJob();
+    }
+
+
+    public static ConnectionManager getInstance() {
+
+        if (instance == null) {
+            instance = new ConnectionManager();
+        }
+        return instance;
+    }
+
+    public static List<ConnectionReceiverListener> connectionReceiverListeners;
+
+
+    public static void addConnectionListener(ConnectionReceiverListener listener) {
+        connectionReceiverListeners.add(listener);
+    }
+
+    private void startNetworkJob(Context context){
+        JobScheduler js =
+                (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo job = new JobInfo.Builder(
+                MY_BACKGROUND_JOB,
+                new ComponentName(context, MyJobService.class))
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setRequiresCharging(true)
+                .build();
+        js.schedule(job);
+    }
+
+
+    public boolean isConnected() {
+        ConnectivityManager
+                cm = (ConnectivityManager) BreadWalletApp.getBreadContext().getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+    }
+
+
+    public interface ConnectionReceiverListener {
+        void onNetworkConnectionChanged(boolean isConnected);
+    }
+
+}
