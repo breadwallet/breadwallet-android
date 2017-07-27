6eef6866855c968ade0dcc687b1fee3a642131ce
Gutyn <Mishulika92@gmail.com>
2017-07-27 15:50:14 -0700

    show timer in disabled view

--- app/src/main/java/com/breadwallet/presenter/activities/DisabledActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/DisabledActivity.java
@@ -3,7 +3,10 @@ package com.breadwallet.presenter.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -27,12 +30,14 @@ import static com.platform.HTTPServer.URL_SUPPORT;
 
 
 public class DisabledActivity extends BRActivity {
-
+    private static final String TAG = DisabledActivity.class.getName();
     private TextView untilLabel;
     private TextView disabled;
     private TextView attempts;
     private ConstraintLayout layout;
     private Button resetButton;
+    private CountDownTimer timer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +50,7 @@ public class DisabledActivity extends BRActivity {
         disabled = (TextView) findViewById(R.id.disabled);
         attempts = (TextView) findViewById(R.id.attempts_label);
         resetButton = (Button) findViewById(R.id.reset_button);
+
 
         ImageButton faq = (ImageButton) findViewById(R.id.faq_button);
 
@@ -73,22 +79,23 @@ public class DisabledActivity extends BRActivity {
         layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (AuthManager.getInstance().isWalletDisabled(DisabledActivity.this)) {
-                    SpringAnimator.failShakeAnimation(DisabledActivity.this, disabled);
-                } else {
-                    BRAnimator.startBreadActivity(DisabledActivity.this, true);
-                }
+                refresh();
             }
         });
 
-        double waitTimeMinutes = getIntent().getDoubleExtra("waitTimeMinutes", 0);
-        if (waitTimeMinutes == 0) throw new IllegalArgumentException("can't be 0");
-        int failCount = KeyStoreManager.getFailCount(this);
 
-        String formattedDate = Utils.formatTimeStamp((long) (System.currentTimeMillis() + waitTimeMinutes * 60 * 1000), "hh:mm a");
-        untilLabel.setText(String.format("until: %s", formattedDate));
+        int failCount = KeyStoreManager.getFailCount(this);
+        untilLabel.setText("");
         attempts.setText(String.format(Locale.getDefault(), "%d attempts remaining", 8 - failCount));
 
+    }
+
+    private void refresh() {
+        if (AuthManager.getInstance().isWalletDisabled(DisabledActivity.this)) {
+            SpringAnimator.failShakeAnimation(DisabledActivity.this, disabled);
+        } else {
+            BRAnimator.startBreadActivity(DisabledActivity.this, true);
+        }
     }
 
     @Override
@@ -100,11 +107,32 @@ public class DisabledActivity extends BRActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        final double waitTimeMinutes = getIntent().getDoubleExtra("waitTimeMinutes", 0);
+        if (waitTimeMinutes == 0) throw new IllegalArgumentException("can't be 0");
+        int seconds = (int) waitTimeMinutes * 60;
+        timer = new CountDownTimer(seconds * 1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                long durationSeconds = (millisUntilFinished / 1000);
+                untilLabel.setText(String.format(Locale.getDefault(),"%02d:%02d:%02d", durationSeconds / 3600,
+                        (durationSeconds % 3600) / 60, (durationSeconds % 60)));
+            }
+
+            public void onFinish() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refresh();
+                    }
+                }, 1000);
+
+            }
+        }.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        timer.cancel();
 
     }
 
