8908cd8d95ad94c7b3d4630f1ea754e92d3da91c
Gutyn <Mishulika92@gmail.com>
2017-08-18 14:42:20 -0700

    BCH UI activity

--- app/src/main/java/com/breadwallet/presenter/activities/settings/WithdrawBchActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/settings/WithdrawBchActivity.java
@@ -1,27 +1,33 @@
 package com.breadwallet.presenter.activities.settings;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.breadwallet.R;
+import com.breadwallet.presenter.activities.BreadActivity;
 import com.breadwallet.presenter.activities.util.ActivityUTILS;
 import com.breadwallet.presenter.activities.util.BRActivity;
 import com.breadwallet.presenter.customviews.BRDialogView;
+import com.breadwallet.presenter.customviews.BRToast;
 import com.breadwallet.tools.animation.BRAnimator;
 import com.breadwallet.tools.animation.BreadDialog;
+import com.breadwallet.tools.manager.BRClipboardManager;
 import com.breadwallet.tools.manager.BRSharedPrefs;
-import com.breadwallet.tools.util.BRConstants;
 import com.breadwallet.wallet.BRPeerManager;
-
-import static com.platform.HTTPServer.URL_SUPPORT;
 
 public class WithdrawBchActivity extends BRActivity {
     private static final String TAG = WithdrawBchActivity.class.getName();
-    private Button scanButton;
+    private Button scan; //scan
+    private Button paste; //paste
+    private Button send; //button_send
+    private EditText addressEdit; //address_edit
+    private TextView txHash; //tx_hash
+    private static TextView txIdLabel;
+
     public static boolean appVisible = false;
     private static WithdrawBchActivity app;
 
@@ -38,22 +44,35 @@ public class WithdrawBchActivity extends BRActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_withdraw_bch);
 
-        ImageButton faq = (ImageButton) findViewById(R.id.faq_button);
+//        ImageButton faq = (ImageButton) findViewById(R.id.faq_button);
+//
+//        faq.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Activity app = WithdrawBchActivity.this;
+//                Intent intent = new Intent(app, WebViewActivity.class);
+//                intent.putExtra("url", URL_SUPPORT);
+//                intent.putExtra("articleId", BRConstants.reScan);
+//                app.startActivity(intent);
+//                app.overridePendingTransition(R.anim.enter_from_bottom, R.anim.empty_300);
+//            }
+//        });
+        txIdLabel = (TextView) findViewById(R.id.tx_label);
+        paste = (Button) findViewById(R.id.paste);
+        send = (Button) findViewById(R.id.button_send);
+        scan = (Button) findViewById(R.id.scan);
+        txHash = (TextView) findViewById(R.id.tx_hash);
+        addressEdit = (EditText) findViewById(R.id.address_edit);
 
-        faq.setOnClickListener(new View.OnClickListener() {
+        txHash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Activity app = WithdrawBchActivity.this;
-                Intent intent = new Intent(app, WebViewActivity.class);
-                intent.putExtra("url", URL_SUPPORT);
-                intent.putExtra("articleId", BRConstants.reScan);
-                app.startActivity(intent);
-                app.overridePendingTransition(R.anim.enter_from_bottom, R.anim.empty_300);
+                BRClipboardManager.putClipboard(WithdrawBchActivity.this, txHash.getText().toString().trim());
+                BRToast.showCustomToast(WithdrawBchActivity.this, "Transaction Id copied", BreadActivity.screenParametersPoint.y / 2, Toast.LENGTH_LONG, 0);
             }
         });
 
-        scanButton = (Button) findViewById(R.id.button_scan);
-        scanButton.setOnClickListener(new View.OnClickListener() {
+        scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!BRAnimator.isClickAllowed()) return;
