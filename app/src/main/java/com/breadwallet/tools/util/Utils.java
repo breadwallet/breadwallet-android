1b0eb8b8e32ae5baf91f89e88f907aee99b350e5
Gutyn <Mishulika92@gmail.com>
2017-09-25 12:07:03 -0700

    fix the ui address bug inconsistency

--- app/src/main/java/com/breadwallet/tools/util/Utils.java
+++ app/src/main/java/com/breadwallet/tools/util/Utils.java
@@ -230,7 +230,7 @@ public class Utils {
             }
         }
         String release = Build.VERSION.RELEASE;
-        return String.format(Locale.getDefault(), "%s/%d %s %s/%s", "breadwallet", versionNumber, cfnetwork, "Android", release);
+        return String.format(Locale.getDefault(), "%s/%d %s %s/%s", "Bread", versionNumber, cfnetwork, "Android", release);
     }
 
     public static String reverseHex(String hex) {
