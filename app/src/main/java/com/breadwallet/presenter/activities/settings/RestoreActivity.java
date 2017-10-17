5dcb71db257e4d194cd1d8b0cdfefabaf399e1d6
Gutyn <Mishulika92@gmail.com>
2017-10-16 17:13:52 -0700

    fix bch text alignment and wrapping

--- app/src/main/java/com/breadwallet/presenter/activities/settings/RestoreActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/settings/RestoreActivity.java
@@ -1,14 +1,13 @@
-package com.breadwallet.presenter.activities;
+package com.breadwallet.presenter.activities.settings;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.breadwallet.R;
+import com.breadwallet.presenter.activities.InputWordsActivity;
 import com.breadwallet.presenter.activities.util.ActivityUTILS;
 import com.breadwallet.presenter.activities.util.BRActivity;
 import com.breadwallet.tools.animation.BRAnimator;
