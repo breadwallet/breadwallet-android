59ac748b72541f89b2fb9b7131e5deebcf4b9e3c
Gutyn <Mishulika92@gmail.com>
2017-11-08 16:43:57 -0800

    fix limit screen linking

--- app/src/main/java/com/breadwallet/presenter/activities/settings/FingerprintActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/settings/FingerprintActivity.java
@@ -109,7 +109,7 @@ public class FingerprintActivity extends BRActivity {
                 ds.setUnderlineText(false);
             }
         };
-        ss.setSpan(clickableSpan, limitInfo.getText().length() - 33, limitInfo.getText().length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(clickableSpan, limitInfo.getText().toString().lastIndexOf(" "), limitInfo.getText().length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         limitInfo.setText(ss);
         limitInfo.setMovementMethod(LinkMovementMethod.getInstance());
