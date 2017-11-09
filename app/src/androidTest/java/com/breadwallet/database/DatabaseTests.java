b05096c853b7c04ddc716231e8449b71ff714f69
Gutyn <Mishulika92@gmail.com>
2017-11-09 14:34:18 -0800

    add asynchronous tests

--- app/src/androidTest/java/com/breadwallet/database/DatabaseTests.java
+++ app/src/androidTest/java/com/breadwallet/database/DatabaseTests.java
@@ -6,6 +6,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import com.breadwallet.presenter.activities.BreadActivity;
+import com.breadwallet.presenter.activities.intro.IntroActivity;
 import com.breadwallet.presenter.entities.BRMerkleBlockEntity;
 import com.breadwallet.presenter.entities.BRPeerEntity;
 import com.breadwallet.presenter.entities.BRTransactionEntity;
@@ -16,6 +17,7 @@ import com.breadwallet.tools.sqlite.CurrencyDataSource;
 import com.breadwallet.tools.sqlite.MerkleBlockDataSource;
 import com.breadwallet.tools.sqlite.PeerDataSource;
 import com.breadwallet.tools.sqlite.TransactionDataSource;
+import com.breadwallet.tools.threads.BRExecutor;
 import com.breadwallet.tools.util.BRConstants;
 import com.platform.interfaces.KVStoreAdaptor;
 import com.platform.kvstore.CompletionObject;
@@ -37,6 +39,8 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -69,9 +73,10 @@ import java.util.Map;
 
 public class DatabaseTests {
     public static final String TAG = DatabaseTests.class.getName();
+    final CountDownLatch signal = new CountDownLatch(1000);
 
     @Rule
-    public ActivityTestRule<BreadActivity> mActivityRule = new ActivityTestRule<>(BreadActivity.class);
+    public ActivityTestRule<IntroActivity> mActivityRule = new ActivityTestRule<>(IntroActivity.class);
 
     @Before
     public void setUp() {
@@ -125,6 +130,69 @@ public class DatabaseTests {
         Assert.assertEquals(cs.get(0).name, "OmiseGo");
         Assert.assertEquals(cs.get(0).code, "OMG");
         Assert.assertEquals(cs.get(0).rate, 8.43f, 0);
+
+    }
+
+    private synchronized void done() {
+        signal.countDown();
+    }
+
+    @Test
+    public void testAsynchronousInserts() {
+        for (int i = 0; i < 1000; i++) {
+            final int finalI = i;
+            BRExecutor.getInstance().forLightWeightBackgroundTasks().execute(new Runnable() {
+                @Override
+                public void run() {
+                    TransactionDataSource tds = TransactionDataSource.getInstance(mActivityRule.getActivity());
+                    tds.putTransaction(new BRTransactionEntity(String.valueOf(finalI).getBytes(), finalI, finalI, String.valueOf(finalI)));
+                    done();
+                }
+            });
+        }
+        try {
+            signal.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.e(TAG, "testAsynchronousInserts: Done waiting!");
+        TransactionDataSource tds = TransactionDataSource.getInstance(mActivityRule.getActivity());
+        List<BRTransactionEntity> txs = tds.getAllTransactions();
+        Assert.assertNotNull(txs);
+        Assert.assertEquals(txs.size(), 1000);
+
+
+//        MerkleBlockDataSource mds = MerkleBlockDataSource.getInstance(mActivityRule.getActivity());
+//        mds.putMerkleBlocks(new BlockEntity[]{new BlockEntity("SOme cool stuff".getBytes(), 123343)});
+//        List<BRMerkleBlockEntity> ms = mds.getAllMerkleBlocks();
+//        Assert.assertNotNull(ms);
+//        Assert.assertEquals(ms.size(), 1);
+//        Assert.assertArrayEquals(ms.get(0).getBuff(), "SOme cool stuff".getBytes());
+//        Assert.assertEquals(ms.get(0).getBlockHeight(), 123343);
+//
+//        PeerDataSource pds = PeerDataSource.getInstance(mActivityRule.getActivity());
+//        pds.putPeers(new PeerEntity[]{new PeerEntity("someAddress".getBytes(), "somePort".getBytes(), "someTimestamp".getBytes())});
+//        List<BRPeerEntity> ps = pds.getAllPeers();
+//        Assert.assertNotNull(ps);
+//        Assert.assertEquals(ps.size(), 1);
+//        Assert.assertArrayEquals(ps.get(0).getAddress(), "someAddress".getBytes());
+//        Assert.assertArrayEquals(ps.get(0).getPort(), "somePort".getBytes());
+//        Assert.assertArrayEquals(ps.get(0).getTimeStamp(), "someTimestamp".getBytes());
+//
+//        CurrencyDataSource cds = CurrencyDataSource.getInstance(mActivityRule.getActivity());
+//        List<CurrencyEntity> toInsert = new ArrayList<>();
+//        CurrencyEntity ent = new CurrencyEntity();
+//        ent.code = "OMG";
+//        ent.name = "OmiseGo";
+//        ent.rate = 8.43f;
+//        toInsert.add(ent);
+//        cds.putCurrencies(toInsert);
+//        List<CurrencyEntity> cs = cds.getAllCurrencies();
+//        Assert.assertNotNull(cs);
+//        Assert.assertEquals(cs.size(), 1);
+//        Assert.assertEquals(cs.get(0).name, "OmiseGo");
+//        Assert.assertEquals(cs.get(0).code, "OMG");
+//        Assert.assertEquals(cs.get(0).rate, 8.43f, 0);
 
     }
 
