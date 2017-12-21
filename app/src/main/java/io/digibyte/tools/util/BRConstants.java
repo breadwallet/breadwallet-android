d81d4d52e091a2f8410b681d81cae30c2e77d6a7
digicontributer <esotericizm88@gmail.com>
2017-12-21 14:12:22 +1000

    more renaming

--- app/src/main/java/io/digibyte/tools/util/BRConstants.java
+++ app/src/main/java/io/digibyte/tools/util/BRConstants.java
@@ -71,7 +71,7 @@ public class BRConstants {
     public static final String FEE_KB_PREFS = "feeKb";
     public static final String ECONOMY_FEE_KB_PREFS = "EconomyFeeKb";
     public static final String LITTLE_CIRCLE = "\u2022";
-    public static String SUPPORT_EMAIL = "support@breadwallet.com";
+    public static String SUPPORT_EMAIL = "support@digibyte.io";
 
     public static final int ONE_BITCOIN = 100000000;
     public static final int HUNDRED_BITS = 10000000;
