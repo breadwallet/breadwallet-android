ac75b707009ae33985dc402f24cc6210d26d339e
Jade Byfield <jjbyfield@gmail.com>
2018-01-16 18:57:20 -0500

    Initial commit test. Setting up new Currency activity

---
+++ app/src/main/java/com/breadwallet/presenter/activities/CurrencyActivity.java
@@ -0,0 +1,13 @@
+package com.breadwallet.presenter.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by byfieldj on 1/16/18.
+ *
+ *
+ * This activity will display pricing and transaction information for any currency the user has access to
+ */
+
+public class CurrencyActivity extends Activity {
+}
