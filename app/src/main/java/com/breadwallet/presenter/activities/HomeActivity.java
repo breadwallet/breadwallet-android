22b80569b7bd35bbcadc8a1c80b3260b49acce74
Gutyn <Mishulika92@gmail.com>
2018-02-12 13:22:52 -0800

    more conflicts

--- app/src/main/java/com/breadwallet/presenter/activities/HomeActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/HomeActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.breadwallet.R;
-import com.breadwallet.presenter.activities.intro.IntroActivity;
 import com.breadwallet.tools.manager.BRSharedPrefs;
 
 /**
@@ -17,14 +16,14 @@ import com.breadwallet.tools.manager.BRSharedPrefs;
  * Dummy Home activity to simulate navigating to and from the new Currency screen
  */
 
-public class TestHomeActivity extends Activity {
+public class HomeActivity extends Activity {
 
     private RelativeLayout mBitcoinCard;
 
     private RelativeLayout mBchCard;
-    private static TestHomeActivity app;
+    private static HomeActivity app;
 
-    public static TestHomeActivity getApp() {
+    public static HomeActivity getApp() {
         return app;
     }
 
@@ -32,7 +31,7 @@ public class TestHomeActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_home);
+        setContentView(R.layout.activity_home);
 
         mBitcoinCard = findViewById(R.id.bitcoin_card);
         mBchCard = findViewById(R.id.bitcoin_cash_card);
@@ -40,8 +39,8 @@ public class TestHomeActivity extends Activity {
         mBitcoinCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                BRSharedPrefs.putCurrentWalletIso(TestHomeActivity.this, "BTC");
-                Intent newIntent = new Intent(TestHomeActivity.this, WalletActivity.class);
+                BRSharedPrefs.putCurrentWalletIso(HomeActivity.this, "BTC");
+                Intent newIntent = new Intent(HomeActivity.this, WalletActivity.class);
                 startActivity(newIntent);
             }
         });
@@ -49,8 +48,8 @@ public class TestHomeActivity extends Activity {
         mBchCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                BRSharedPrefs.putCurrentWalletIso(TestHomeActivity.this, "BCH");
-                Intent newIntent = new Intent(TestHomeActivity.this, WalletActivity.class);
+                BRSharedPrefs.putCurrentWalletIso(HomeActivity.this, "BCH");
+                Intent newIntent = new Intent(HomeActivity.this, WalletActivity.class);
                 startActivity(newIntent);
             }
         });
@@ -60,7 +59,7 @@ public class TestHomeActivity extends Activity {
     protected void onResume() {
         super.onResume();
         app = this;
-        BRSharedPrefs.putCurrentWalletIso(TestHomeActivity.this, "");
+        BRSharedPrefs.putCurrentWalletIso(HomeActivity.this, "");
     }
 
 }
