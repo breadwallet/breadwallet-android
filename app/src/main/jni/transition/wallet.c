f8fa9b7cdafe2239dd6f12d54713fa7f1d4441fa
Neil Vallon <neilvallon@gmail.com>
2018-03-14 18:03:32 -0500

    make build

--- app/src/main/jni/transition/wallet.c
+++ app/src/main/jni/transition/wallet.c
@@ -22,6 +22,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#include <stdlib.h>
+
 #include "wallet.h"
 #include "PeerManager.h"
 #include "BRPeerManager.h"
