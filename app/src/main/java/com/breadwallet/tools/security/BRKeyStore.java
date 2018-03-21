b2ab739798ff38f5b3945370a86367e62d590e8e
Gutyn <Mishulika92@gmail.com>
2018-03-21 15:23:00 -0700

    fix KeyStore decryption bug

--- app/src/main/java/com/breadwallet/tools/security/BRKeyStore.java
+++ app/src/main/java/com/breadwallet/tools/security/BRKeyStore.java
@@ -286,7 +286,6 @@ public class BRKeyStore {
                     }
                 } catch (IllegalBlockSizeException | BadPaddingException e) {
                     e.printStackTrace();
-                    throw new RuntimeException("failed to decrypt data: " + e.getMessage());
                 }
             }
             //no new format data, get the old one and migrate it to the new format
