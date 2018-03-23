96954b3c4baf5d24985c3cbb01edac0063f65116
Gutyn <Mishulika92@gmail.com>
2018-03-23 10:19:00 -0700

    reverse the fix that was a bad fix, very disappointing fix

--- app/src/main/java/com/breadwallet/tools/security/BRKeyStore.java
+++ app/src/main/java/com/breadwallet/tools/security/BRKeyStore.java
@@ -286,6 +286,7 @@ public class BRKeyStore {
                     }
                 } catch (IllegalBlockSizeException | BadPaddingException e) {
                     e.printStackTrace();
+                    throw new RuntimeException("failed to decrypt data: " + e.getMessage());
                 }
             }
             //no new format data, get the old one and migrate it to the new format
