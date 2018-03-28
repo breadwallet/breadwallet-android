883714e2a94183ea75f6f06cd6dfe032b48878c9
Gutyn <Mishulika92@gmail.com>
2018-03-28 16:14:13 -0700

    fix Settings UI and adaptor

--- app/src/main/java/com/breadwallet/presenter/entities/BRSettingsItem.java
+++ app/src/main/java/com/breadwallet/presenter/entities/BRSettingsItem.java
@@ -29,15 +29,17 @@ import android.view.View;
 public class BRSettingsItem {
 
     public boolean isSection;
+    public boolean showChevron;
     public String title;
     public String addonText;
     public View.OnClickListener listener;
 
-    public BRSettingsItem(String title, String addonText, View.OnClickListener listener, boolean isSection) {
+    public BRSettingsItem(String title, String addonText, View.OnClickListener listener, boolean isSection, boolean showChevron) {
         this.title = title;
         this.addonText = addonText;
         this.listener = listener;
         this.isSection = isSection;
+        this.showChevron = showChevron;
     }
 
 }
