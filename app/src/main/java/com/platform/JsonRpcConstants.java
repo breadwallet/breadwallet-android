f0dbe18123fd115b95ff0fbdf3079f271cdb6e69
Gutyn <Mishulika92@gmail.com>
2018-03-29 16:55:51 -0700

    use testnet or mainnet links accordingly

--- app/src/main/java/com/platform/JsonRpcConstants.java
+++ app/src/main/java/com/platform/JsonRpcConstants.java
@@ -1,15 +1,16 @@
 package com.platform;
 
+import com.breadwallet.BuildConfig;
+
 /**
  * Created by byfieldj on 3/26/18.
  */
 
 public class JsonRpcConstants {
-
-
     // Ethereum rpc endpoint
-    public static final String BRD_ETH_RPC_ENDPOINT = "/ethq/ropsten/proxy";
-    public static final String ETH_RPC_TX_LIST = "https://ropsten.etherscan.io/api?module=account&action=txlist&address=%s";
-
+    public static final String BRD_ETH_RPC_ENDPOINT = BuildConfig.BITCOIN_TESTNET ? "/ethq/ropsten/proxy" : "ethq/mainnet/proxy";
+    public static final String ETH_RPC_TX_LIST = BuildConfig.BITCOIN_TESTNET ?
+            "https://ropsten.etherscan.io/api?module=account&action=txlist&address=%s" :
+            "https://api.etherscan.io/api?module=account&action=txlist&address=%s";
 
 }
