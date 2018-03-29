6ed31618a6e9070ab8bd6f733ebb7f5c217e2e32
Jade Byfield <jjbyfield@gmail.com>
2018-03-29 13:13:35 -0400

    Print request body

--- app/src/main/java/com/platform/JsonRpcConstants.java
+++ app/src/main/java/com/platform/JsonRpcConstants.java
@@ -10,9 +10,6 @@ public class JsonRpcConstants {
     // Ethereum rpc endpoint
     public static final String BRD_ETH_RPC_ENDPOINT = "/ethq/ropsten/proxy";
     public static final String ETH_RPC_TX_LIST = "https://ropsten.etherscan.io/api?module=account&action=txlist&address=%s";
-    public static final String ETH_ENDPOINT_ESTIMATE_GAS = "eth_estimateGas";
-    public static final String ETH_ENDPOINT_GAS_PRICE = "eth_gasPrice";
-    public static final String ETH_ENDPOINT_GET_BALANCE = "eth_getBalance";
 
 
 }
