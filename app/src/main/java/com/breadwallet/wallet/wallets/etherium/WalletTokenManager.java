e74cf4a5f99169f8ced60bb302daa74921df082f
Gutyn <Mishulika92@gmail.com>
2018-04-16 14:09:53 -0700

    implement token functions

--- app/src/main/java/com/breadwallet/wallet/wallets/etherium/WalletTokenManager.java
+++ app/src/main/java/com/breadwallet/wallet/wallets/etherium/WalletTokenManager.java
@@ -2,10 +2,14 @@ package com.breadwallet.wallet.wallets.etherium;
 
 import android.content.Context;
 
+import com.breadwallet.core.ethereum.BREthereumAmount;
 import com.breadwallet.core.ethereum.BREthereumToken;
+import com.breadwallet.core.ethereum.BREthereumTransaction;
 import com.breadwallet.core.ethereum.BREthereumWallet;
 import com.breadwallet.presenter.entities.CurrencyEntity;
 import com.breadwallet.presenter.entities.TxUiHolder;
+import com.breadwallet.tools.manager.BRSharedPrefs;
+import com.breadwallet.tools.threads.executor.BRExecutor;
 import com.breadwallet.tools.util.Utils;
 import com.breadwallet.wallet.abstracts.BaseAddress;
 import com.breadwallet.wallet.abstracts.BaseTransaction;
@@ -16,6 +20,7 @@ import com.breadwallet.wallet.abstracts.OnTxStatusUpdatedListener;
 import com.breadwallet.wallet.abstracts.SyncListener;
 import com.breadwallet.wallet.configs.WalletSettingsConfiguration;
 import com.breadwallet.wallet.configs.WalletUiConfiguration;
+import com.breadwallet.wallet.wallets.CryptoTransaction;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -48,17 +53,20 @@ import java.util.List;
 public class WalletTokenManager implements BaseWalletManager {
 
     private WalletEthManager mWalletEthManager;
-    private BREthereumWallet mWalletToken ;
+    private BREthereumWallet mWalletToken;
 
 
     private List<OnBalanceChangedListener> balanceListeners = new ArrayList<>();
     private List<OnTxStatusUpdatedListener> txStatusUpdatedListeners = new ArrayList<>();
     private List<SyncListener> syncListeners = new ArrayList<>();
     private List<OnTxListModified> txModifiedListeners = new ArrayList<>();
+    private WalletUiConfiguration uiConfig;
 
-    public WalletTokenManager(WalletEthManager walletEthManager, String contractAddress) {
+    public WalletTokenManager(WalletEthManager walletEthManager, String startColor, String endColor, String contractAddress) {
         this.mWalletEthManager = walletEthManager;
-       mWalletToken = walletEthManager.getNode().createWallet(BREthereumToken.tokenBRD);
+        uiConfig = new WalletUiConfiguration(startColor, endColor, false);
+
+        mWalletToken = walletEthManager.getNode().createWallet(BREthereumToken.tokenBRD);
     }
 
     @Override
@@ -144,12 +152,20 @@ public class WalletTokenManager implements BaseWalletManager {
 
     @Override
     public BigDecimal getTxFee(BaseTransaction tx) {
-        return null;
+        return new BigDecimal(tx.getEtherTx().getGasLimit())
+                .multiply(new BigDecimal(tx.getEtherTx().getGasPrice(BREthereumAmount.Unit.ETHER_WEI)));
     }
 
     @Override
     public BigDecimal getEstimatedFee(BigDecimal amount, String address) {
-        return null;
+        BigDecimal fee;
+        if (amount == null) return null;
+        if (amount.compareTo(new BigDecimal(0)) == 0) {
+            fee = new BigDecimal(0);
+        } else {
+            fee = new BigDecimal(mWalletToken.transactionEstimatedFee(amount.toPlainString()));
+        }
+        return fee;
     }
 
     @Override
@@ -159,62 +175,69 @@ public class WalletTokenManager implements BaseWalletManager {
 
     @Override
     public BaseAddress getTxAddress(BaseTransaction tx) {
-        return null;
+        return mWalletEthManager.getTxAddress(tx);
     }
 
     @Override
     public BigDecimal getMaxOutputAmount(Context app) {
-        return null;
+        return mWalletEthManager.getMaxOutputAmount(app);
     }
 
     @Override
     public BigDecimal getMinOutputAmount(Context app) {
-        return null;
+        return mWalletEthManager.getMinOutputAmount(app);
     }
 
     @Override
     public BigDecimal getTransactionAmount(BaseTransaction tx) {
-        return null;
+        return mWalletEthManager.getTransactionAmount(tx);
     }
 
     @Override
     public BigDecimal getMinOutputAmountPossible() {
-        return null;
+        return mWalletEthManager.getMinOutputAmountPossible();
     }
 
     @Override
     public void updateFee(Context app) {
-
+        //no need
     }
 
     @Override
     public void refreshAddress(Context app) {
-
+        //no need
     }
 
     @Override
-    public void refreshCachedBalance(Context app) {
-
+    public void refreshCachedBalance(final Context app) {
+        BRExecutor.getInstance().forLightWeightBackgroundTasks().execute(new Runnable() {
+            @Override
+            public void run() {
+                BigDecimal balance = new BigDecimal(mWalletToken.getBalance());
+                BRSharedPrefs.putCachedBalance(app, getIso(app), balance);
+            }
+        });
     }
 
     @Override
     public List<TxUiHolder> getTxUiHolders(Context app) {
+        //todo implement
         return null;
     }
 
     @Override
     public boolean containsAddress(String address) {
-        return false;
+        return mWalletEthManager.containsAddress(address);
     }
 
     @Override
     public boolean addressIsUsed(String address) {
-        return false;
+        return mWalletEthManager.addressIsUsed(address);
     }
 
     @Override
     public BaseAddress createAddress(String address) {
-        return null;
+        return mWalletEthManager.createAddress(address);
     }
 
     @Override
@@ -224,12 +247,12 @@ public class WalletTokenManager implements BaseWalletManager {
 
     @Override
     public String getSymbol(Context app) {
-        return null;
+        return mWalletToken.getToken().getSymbol();
     }
 
     @Override
     public String getIso(Context app) {
-        return null;
+        return mWalletToken.getToken().getSymbol();
     }
 
     @Override
@@ -239,7 +262,7 @@ public class WalletTokenManager implements BaseWalletManager {
 
     @Override
     public String getName(Context app) {
-        return null;
+        return mWalletToken.getToken().getName();
     }
 
     @Override
@@ -249,77 +272,83 @@ public class WalletTokenManager implements BaseWalletManager {
 
     @Override
     public BaseAddress getReceiveAddress(Context app) {
-        return null;
+        return mWalletEthManager.getReceiveAddress(app);
     }
 
     @Override
     public BaseTransaction createTransaction(BigDecimal amount, String address) {
-        return null;
+        BREthereumTransaction tx = mWalletToken.createTransaction(address, amount.toPlainString(), BREthereumAmount.Unit.ETHER_WEI);
+        return new CryptoTransaction(tx);
     }
 
     @Override
     public String decorateAddress(Context app, String addr) {
-        return null;
+        return addr;
     }
 
     @Override
     public String undecorateAddress(Context app, String addr) {
-        return null;
+        return addr;
     }
 
     @Override
     public int getMaxDecimalPlaces(Context app) {
-        return 0;
+        return 5;
     }
 
     @Override
     public BigDecimal getCachedBalance(Context app) {
-        return null;
+        return BRSharedPrefs.getCachedBalance(app, getIso(app));
+
     }
 
     @Override
     public BigDecimal getTotalSent(Context app) {
-        return null;
+        return mWalletEthManager.getTotalSent(app);
     }
 
     @Override
     public void wipeData(Context app) {
-
+        //Not needed for Tokens
     }
 
     @Override
     public void syncStarted() {
-
+        //Not needed for Tokens
     }
 
     @Override
     public void syncStopped(String error) {
-
+        //Not needed for Tokens
     }
 
     @Override
     public boolean networkIsReachable() {
-        return false;
+        return mWalletEthManager.networkIsReachable();
     }
 
     @Override
     public void setCachedBalance(Context app, BigDecimal balance) {
-
+        BRSharedPrefs.putCachedBalance(app, getIso(app), balance);
+        for (OnBalanceChangedListener listener : balanceListeners) {
+            if (listener != null) listener.onBalanceChanged(getIso(app), balance);
+        }
     }
 
     @Override
     public BigDecimal getMaxAmount(Context app) {
-        return null;
+        return mWalletEthManager.getMaxAmount(app);
     }
 
     @Override
     public WalletUiConfiguration getUiConfiguration() {
-        return null;
+        return uiConfig;
     }
 
     @Override
     public WalletSettingsConfiguration getSettingsConfiguration() {
-        return null;
+        //no settings for tokens, so empty
+        return new WalletSettingsConfiguration();
     }
 
     @Override
