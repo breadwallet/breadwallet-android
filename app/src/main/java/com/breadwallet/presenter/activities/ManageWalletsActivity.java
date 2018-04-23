8b587e736bc91a1d92ee92271780fc230ebbb34b
Jade Byfield <jade@Jades-MBP.home>
2018-04-23 18:53:48 -0400

    Manage Tokens, need to save sorted order

---
+++ app/src/main/java/com/breadwallet/presenter/activities/ManageWalletsActivity.java
@@ -0,0 +1,118 @@
+package com.breadwallet.presenter.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+
+import com.breadwallet.R;
+import com.breadwallet.core.ethereum.BREthereumToken;
+import com.breadwallet.presenter.activities.util.BRActivity;
+import com.breadwallet.presenter.entities.TokenItem;
+import com.breadwallet.tools.adapter.ManageTokenListAdapter;
+import com.breadwallet.tools.animation.SimpleItemTouchHelperCallback;
+import com.breadwallet.tools.threads.executor.BRExecutor;
+import com.platform.entities.TokenListMetaData;
+import com.platform.tools.KVStoreManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManageWalletsActivity extends BRActivity {
+
+    private ManageTokenListAdapter mAdapter;
+    private RecyclerView mTokenList;
+    private List<TokenListMetaData.TokenInfo> mTokens;
+    private ItemTouchHelper mItemTouchHelper;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_wallets);
+
+        mTokenList = findViewById(R.id.token_list);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final ArrayList<TokenItem> tokenItems = new ArrayList<>();
+
+
+        BRExecutor.getInstance().forLightWeightBackgroundTasks().execute(new Runnable() {
+            @Override
+            public void run() {
+                mTokens = KVStoreManager.getInstance().getTokenListMetaData(ManageWalletsActivity.this).enabledCurrencies;
+
+
+                for (int i = 0; i < mTokens.size(); i++) {
+
+                    TokenListMetaData.TokenInfo tokenInfo = mTokens.get(i);
+                    TokenListMetaData.TokenInfo info = mTokens.get(i);
+                    TokenItem tokenItem;
+                    Log.d("ManageWalletsActivity", "Looking up -> " + mTokens.get(i).symbol);
+
+                    if (mTokens.get(i).symbol.equalsIgnoreCase("btc")) {
+                        tokenItem = new TokenItem(null, tokenInfo.symbol, "Bitcoin", null);
+                    } else if (mTokens.get(i).symbol.equalsIgnoreCase("bch")) {
+                        tokenItem = new TokenItem(null, tokenInfo.symbol, "Bitcoin Cash", null);
+
+                    } else if (mTokens.get(i).symbol.equalsIgnoreCase("eth")) {
+                        tokenItem = new TokenItem(null, tokenInfo.symbol, "Ethereum", null);
+
+                    } else {
+
+                        BREthereumToken tk = BREthereumToken.lookup(info.contractAddress);
+                        tokenItem = new TokenItem(tk.getAddress(), tk.getSymbol(), tk.getName(), null);
+
+                    }
+                    tokenItems.add(tokenItem);
+
+                }
+
+
+            }
+        });
+
+        mAdapter = new ManageTokenListAdapter(ManageWalletsActivity.this, tokenItems, new ManageTokenListAdapter.OnTokenShowOrHideListener() {
+            @Override
+            public void onShowToken(TokenItem token) {
+
+                TokenListMetaData metaData = KVStoreManager.getInstance().getTokenListMetaData(ManageWalletsActivity.this);
+                TokenListMetaData.TokenInfo item = new TokenListMetaData.TokenInfo(token.symbol, true, token.address);
+                if (metaData == null) metaData = new TokenListMetaData(null, null);
+                if (metaData.isCurrencyHidden(item.symbol))
+                    metaData.hiddenCurrencies.remove(item);
+                metaData.enabledCurrencies.add(item);
+                KVStoreManager.getInstance().putTokenListMetaData(ManageWalletsActivity.this, metaData);
+
+
+            }
+
+            @Override
+            public void onHideToken(TokenItem token) {
+
+                TokenListMetaData metaData = KVStoreManager.getInstance().getTokenListMetaData(ManageWalletsActivity.this);
+                TokenListMetaData.TokenInfo item = new TokenListMetaData.TokenInfo(token.symbol, true, token.address);
+                if (metaData == null) metaData = new TokenListMetaData(null, null);
+                if (!metaData.isCurrencyHidden(token.symbol))
+                    metaData.disableCurrency(item.symbol);
+                KVStoreManager.getInstance().putTokenListMetaData(ManageWalletsActivity.this, metaData);
+
+
+            }
+        });
+
+        mTokenList.setLayoutManager(new LinearLayoutManager(ManageWalletsActivity.this));
+        mTokenList.setAdapter(mAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mTokenList);
+    }
+}
---
+++ app/src/main/java/com/breadwallet/tools/adapter/ManageTokenListAdapter.java
@@ -0,0 +1,160 @@
+package com.breadwallet.tools.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.breadwallet.R;
+import com.breadwallet.presenter.customviews.BRText;
+import com.breadwallet.presenter.entities.TokenItem;
+import com.breadwallet.tools.animation.ItemTouchHelperAdapter;
+import com.breadwallet.tools.animation.ItemTouchHelperViewHolder;
+import com.platform.tools.KVStoreManager;
+
+import java.util.ArrayList;
+
+public class ManageTokenListAdapter extends RecyclerView.Adapter<ManageTokenListAdapter.ManageTokenItemViewHolder> implements ItemTouchHelperAdapter {
+
+    private static final String TAG = ManageTokenListAdapter.class.getSimpleName();
+    private Context mContext;
+    private ArrayList<TokenItem> mTokens;
+    private OnTokenShowOrHideListener mListener;
+
+    public interface OnTokenShowOrHideListener{
+
+        void onShowToken(TokenItem item);
+        void onHideToken(TokenItem item);
+    }
+
+    public ManageTokenListAdapter(Context context, ArrayList<TokenItem> tokens, OnTokenShowOrHideListener listener){
+        this.mContext = context;
+        this.mTokens = tokens;
+        this.mListener = listener;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ManageTokenListAdapter.ManageTokenItemViewHolder holder, int position) {
+
+        final TokenItem item = mTokens.get(position);
+        String tickerName = item.symbol.toLowerCase();
+
+        if (tickerName.equals("1st")) {
+            tickerName = "first";
+        }
+
+
+        String iconResourceName = tickerName;
+        int iconResourceId = mContext.getResources().getIdentifier(tickerName, "drawable", mContext.getPackageName());
+
+        holder.tokenName.setText(mTokens.get(position).name);
+        holder.tokenTicker.setText(mTokens.get(position).symbol);
+        try {
+            holder.tokenIcon.setBackground(mContext.getDrawable(iconResourceId));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Error finding icon for -> " + iconResourceName);
+        }
+
+        if(KVStoreManager.getInstance().getTokenListMetaData(mContext).isCurrencyEnabled(item.symbol)){
+            holder.showHide.setBackground(mContext.getDrawable(R.drawable.remove_wallet_button));
+            holder.showHide.setText("Hide");
+            holder.showHide.setTextColor(mContext.getColor(R.color.red));
+        }else{
+            holder.showHide.setBackground(mContext.getDrawable(R.drawable.add_wallet_button));
+            holder.showHide.setText("Show");
+            holder.showHide.setTextColor(mContext.getColor(R.color.dialog_button_positive));
+
+        }
+
+
+            holder.showHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //First, check if this token is hidden on HomeScreen
+                if(!KVStoreManager.getInstance().getTokenListMetaData(mContext).isCurrencyEnabled(item.symbol)){
+                    Log.d(TAG, "Currency is HIDDEN");
+                    holder.showHide.setText("Show");
+                    holder.showHide.setTextColor(mContext.getColor(R.color.dialog_button_positive));
+                    holder.showHide.setBackground(mContext.getDrawable(R.drawable.add_wallet_button));
+                    mListener.onShowToken(item);
+                }else{
+                    Log.d(TAG, "Currency is SHOWING");
+                    holder.showHide.setText("Hide");
+                    holder.showHide.setTextColor(mContext.getColor(R.color.red));
+                    holder.showHide.setBackground(mContext.getDrawable(R.drawable.remove_wallet_button));
+                    mListener.onHideToken(item);
+
+                }
+            }
+        });
+
+    }
+
+    @NonNull
+    @Override
+    public ManageTokenListAdapter.ManageTokenItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+        View convertView = inflater.inflate(R.layout.manage_wallets_list_item, parent, false);
+        return new ManageTokenItemViewHolder(convertView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTokens.size();
+    }
+
+
+    public class ManageTokenItemViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder{
+
+        private ImageButton dragHandle;
+        private BRText tokenTicker;
+        private BRText tokenName;
+        private BRText tokenBalance;
+        private Button showHide;
+        private ImageView tokenIcon;
+
+        public ManageTokenItemViewHolder(View view) {
+            super(view);
+
+            dragHandle = view.findViewById(R.id.drag_icon);
+            tokenTicker = view.findViewById(R.id.token_ticker);
+            tokenName = view.findViewById(R.id.token_name);
+            tokenBalance = view.findViewById(R.id.token_balance);
+            showHide = view.findViewById(R.id.show_hide_button);
+            tokenIcon = view.findViewById(R.id.token_icon);
+        }
+
+        @Override
+        public void onItemClear() {
+
+        }
+
+        @Override
+        public void onItemSelected() {
+
+        }
+    }
+
+
+    @Override
+    public void onItemDismiss(int position) {
+
+    }
+
+    @Override
+    public void onItemMove(int fromPosition, int toPosition) {
+        TokenItem item = mTokens.remove(fromPosition);
+        mTokens.add(toPosition > fromPosition ? toPosition - 1 : toPosition, item);
+        notifyItemMoved(fromPosition, toPosition);
+    }
+}
