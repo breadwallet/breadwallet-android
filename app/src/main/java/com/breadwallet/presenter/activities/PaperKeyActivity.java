43fd0e06baa04af5025880326147767431f840b1
Jade Byfield <jjbyfield@gmail.com>
2018-07-11 20:43:03 +0000

    DROID-699: Alter Paper Key Previous/Next buttons based on position

--- app/src/main/java/com/breadwallet/presenter/activities/PaperKeyActivity.java
+++ app/src/main/java/com/breadwallet/presenter/activities/PaperKeyActivity.java
@@ -8,7 +8,9 @@ import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.SparseArray;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -19,8 +21,8 @@ import com.breadwallet.R;
 import com.breadwallet.presenter.activities.util.BRActivity;
 import com.breadwallet.presenter.customviews.BRDialogView;
 import com.breadwallet.presenter.fragments.FragmentPhraseWord;
-import com.breadwallet.tools.animation.UiUtils;
 import com.breadwallet.tools.animation.BRDialog;
+import com.breadwallet.tools.animation.UiUtils;
 import com.breadwallet.tools.manager.BRReportsManager;
 import com.breadwallet.tools.security.PostAuth;
 import com.breadwallet.tools.util.Utils;
@@ -30,13 +32,18 @@ import java.util.Locale;
 
 public class PaperKeyActivity extends BRActivity {
     private static final String TAG = PaperKeyActivity.class.getName();
-    private ViewPager wordViewPager;
-    private Button nextButton;
-    private Button previousButton;
-    private LinearLayout buttonsLayout;
-    private TextView itemIndexText;
-    private SparseArray<String> wordMap;
-    private ImageButton close;
+
+    private static final int NAVIGATION_BUTTONS_WEIGHT = 1;
+    private static final float BUTTONS_LAYOUT_WEIGHT_SUM_DEFAULT = 2.0f;
+    private static final float BUTTONS_LAYOUT_WEIGHT_SUM_SINGLE = 1.0f;
+
+    private ViewPager mWordViewPager;
+    private Button mNextButton;
+    private Button mPreviousButton;
+    private LinearLayout mButtonsLayout;
+    private TextView mItemIndexTextView;
+    private ImageButton mCloseImageButton;
+    private SparseArray<String> mWordMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,39 +51,46 @@ public class PaperKeyActivity extends BRActivity {
         setContentView(R.layout.activity_paper_key);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
 
-        wordViewPager = findViewById(R.id.phrase_words_pager);
-        wordViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            public void onPageScrollStateChanged(int state) {
-
-            }
-
+        mWordViewPager = findViewById(R.id.phrase_words_pager);
+        mWordViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
             }
 
+            @Override
             public void onPageSelected(int position) {
-                if (position == 0)
-                    setButtonEnabled(false);
-                else
-                    setButtonEnabled(true);
                 updateItemIndexText();
+                if (position == 0) {
+                    setButtonEnabled(false);
+                    updateNavigationButtons(false);
+                } else {
+                    setButtonEnabled(true);
+                    updateNavigationButtons(true);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
 
-        nextButton = findViewById(R.id.next_button);
-        previousButton = findViewById(R.id.previous_button);
-        close = findViewById(R.id.close_button);
-        itemIndexText = findViewById(R.id.item_index_text);
-        buttonsLayout = findViewById(R.id.buttons_layout);
+        mNextButton = findViewById(R.id.next_button);
+        mPreviousButton = findViewById(R.id.previous_button);
+        mCloseImageButton = findViewById(R.id.close_button);
+        mItemIndexTextView = findViewById(R.id.item_index_text);
+        mButtonsLayout = findViewById(R.id.buttons_layout);
+        updateNavigationButtons(false);
 
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 updateWordView(true);
             }
         });
 
-        close.setOnClickListener(new View.OnClickListener() {
+        mCloseImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!UiUtils.isClickAllowed()) return;
@@ -84,21 +98,23 @@ public class PaperKeyActivity extends BRActivity {
                 if (!isDestroyed()) finish();
             }
         });
-        previousButton.setOnClickListener(new View.OnClickListener() {
+
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 updateWordView(false);
 
             }
         });
+
         String cleanPhrase = getIntent().getExtras() == null ? null : getIntent().getStringExtra("phrase");
-        wordMap = new SparseArray<>();
+        mWordMap = new SparseArray<>();
 
         if (Utils.isNullOrEmpty(cleanPhrase)) {
-            throw new RuntimeException(TAG + ": cleanPhrase is null");
+            throw new IllegalArgumentException(TAG + ": cleanPhrase is null");
         }
 
-        String wordArray[ ] = cleanPhrase.split(" ");
+        String[] wordArray = cleanPhrase.split(" ");
 
         if (cleanPhrase.charAt(cleanPhrase.length() - 1) == '\0') {
             BRDialog.showCustomDialog(this, getString(R.string.JailbreakWarnings_title),
@@ -115,29 +131,64 @@ public class PaperKeyActivity extends BRActivity {
             }
             WordPagerAdapter adapter = new WordPagerAdapter(getFragmentManager());
             adapter.setWords(wordArray);
-            wordViewPager.setAdapter(adapter);
+            mWordViewPager.setAdapter(adapter);
             for (int i = 0; i < wordArray.length; i++) {
-                wordMap.append(i, wordArray[i]);
+                mWordMap.append(i, wordArray[i]);
             }
             updateItemIndexText();
         }
     }
 
+    /**
+     * Show or hide the "Previous" button used to navigate the ViewPager.
+     *
+     * @param showPrevious
+     */
+    private void updateNavigationButtons(boolean showPrevious) {
+        LinearLayout.LayoutParams nextButtonParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        if (!showPrevious) {
+            mButtonsLayout.setWeightSum(BUTTONS_LAYOUT_WEIGHT_SUM_SINGLE);
+
+            nextButtonParams.weight = NAVIGATION_BUTTONS_WEIGHT;
+            nextButtonParams.gravity = Gravity.CENTER_HORIZONTAL;
+            nextButtonParams.setMargins((int) getResources().getDimension(R.dimen.margin), 0, (int) getResources().getDimension(R.dimen.margin), 0);
+            mNextButton.setLayoutParams(nextButtonParams);
+            mNextButton.setHeight((int) getResources().getDimension(R.dimen.large_button_height));
+
+            mPreviousButton.setVisibility(View.GONE);
+        } else {
+            mButtonsLayout.setWeightSum(BUTTONS_LAYOUT_WEIGHT_SUM_DEFAULT);
+
+            nextButtonParams.weight = NAVIGATION_BUTTONS_WEIGHT;
+            nextButtonParams.setMargins(0, 0, (int) getResources().getDimension(R.dimen.margin), 0);
+            mNextButton.setLayoutParams(nextButtonParams);
+            mNextButton.setHeight((int) getResources().getDimension(R.dimen.large_button_height));
+
+            LinearLayout.LayoutParams previousButtonParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            previousButtonParams.weight = NAVIGATION_BUTTONS_WEIGHT;
+            previousButtonParams.setMargins((int) getResources().getDimension(R.dimen.margin), 0, 0, 0);
+            mPreviousButton.setLayoutParams(previousButtonParams);
+            mPreviousButton.setVisibility(View.VISIBLE);
+            mPreviousButton.setHeight((int) getResources().getDimension(R.dimen.large_button_height));
+        }
+    }
+
     private void updateWordView(boolean isNext) {
-        int currentIndex = wordViewPager.getCurrentItem();
+        int currentIndex = mWordViewPager.getCurrentItem();
         if (isNext) {
             setButtonEnabled(true);
             if (currentIndex >= 11) {
                 PostAuth.getInstance().onPhraseProveAuth(this, false);
             } else {
-                wordViewPager.setCurrentItem(currentIndex + 1);
+                mWordViewPager.setCurrentItem(currentIndex + 1);
             }
         } else {
             if (currentIndex <= 1) {
-                wordViewPager.setCurrentItem(currentIndex - 1);
+                mWordViewPager.setCurrentItem(currentIndex - 1);
                 setButtonEnabled(false);
             } else {
-                wordViewPager.setCurrentItem(currentIndex - 1);
+                mWordViewPager.setCurrentItem(currentIndex - 1);
             }
         }
     }
@@ -145,13 +196,13 @@ public class PaperKeyActivity extends BRActivity {
     private void setButtonEnabled(boolean b) {
         Resources r = getResources();
         float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, b ? 8 : 0, r.getDisplayMetrics());
-        previousButton.setElevation(px);
-        previousButton.setEnabled(b);
+        mPreviousButton.setElevation(px);
+        mPreviousButton.setEnabled(b);
     }
 
     private void updateItemIndexText() {
-        String text = String.format(Locale.getDefault(), getString(R.string.WritePaperPhrase_step), wordViewPager.getCurrentItem() + 1, wordMap.size());
-        itemIndexText.setText(text);
+        String text = String.format(Locale.getDefault(), getString(R.string.WritePaperPhrase_step), mWordViewPager.getCurrentItem() + 1, mWordMap.size());
+        mItemIndexTextView.setText(text);
     }
 
     @Override
