8359ade275b841ca68227f74b784d578bb128dd4
Jade Byfield <jjbyfield@gmail.com>
2018-12-13 13:23:50 -0500

    Fix regression of DROID-889, but synchronize init() call to fix DROID-1042.

--- app/src/main/java/com/platform/HTTPServer.java
+++ app/src/main/java/com/platform/HTTPServer.java
@@ -80,7 +80,7 @@ public class HTTPServer {
         init();
     }
 
-    private static void init() {
+    private synchronized static void init() {
         middlewares = new LinkedHashSet<>();
         server = new Server(PORT);
         try {
