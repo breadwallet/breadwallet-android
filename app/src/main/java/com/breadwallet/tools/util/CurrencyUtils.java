9225d03c111477355ef542a09ec824b889a26499
Gutyn <Mishulika92@gmail.com>
2018-12-17 16:54:26 -0800

    DROID-1009: Fix Bell, use display currency for check instead of device currency.

--- app/src/main/java/com/breadwallet/tools/util/CurrencyUtils.java
+++ app/src/main/java/com/breadwallet/tools/util/CurrencyUtils.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import com.breadwallet.tools.manager.BRReportsManager;
+import com.breadwallet.tools.manager.BRSharedPrefs;
 import com.breadwallet.wallet.WalletsMaster;
 import com.breadwallet.wallet.abstracts.BaseWalletManager;
 
@@ -122,10 +123,9 @@ public class CurrencyUtils {
 
     }
 
-    public static boolean isBuyNotificationNeeded() {
-        Locale locale = Locale.getDefault();
-        String currencyCode = Currency.getInstance(locale).getCurrencyCode();
-        return KRONE.equalsIgnoreCase(currencyCode) || POUND.equalsIgnoreCase(currencyCode) || EURO.equalsIgnoreCase(currencyCode);
+    public static boolean isBuyNotificationNeeded(Context context) {
+        String fiatCurrencyCode = BRSharedPrefs.getPreferredFiatIso(context);
+        return KRONE.equalsIgnoreCase(fiatCurrencyCode) || POUND.equalsIgnoreCase(fiatCurrencyCode) || EURO.equalsIgnoreCase(fiatCurrencyCode);
     }
 
 }
