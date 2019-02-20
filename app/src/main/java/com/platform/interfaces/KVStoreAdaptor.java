7f9744fb97164c8a5e93d18bc8da9583f15dc9bc
Pablo Budelli <pablo.budelli@breadwallet.com>
2019-02-20 19:15:27 +0000

    DROID-1066: Refactor ReplicatedKVStore to follow the team's coding standards and add Javadocs to KVStoreAdaptor.

--- app/src/main/java/com/platform/interfaces/KVStoreAdaptor.java
+++ app/src/main/java/com/platform/interfaces/KVStoreAdaptor.java
@@ -1,9 +1,3 @@
-package com.platform.interfaces;
-
-import com.platform.kvstore.CompletionObject;
-
-import static android.R.attr.value;
-
 /**
  * BreadWallet
  * <p/>
@@ -28,16 +22,57 @@ import static android.R.attr.value;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+package com.platform.interfaces;
+
+import com.platform.kvstore.CompletionObject;
+
+/**
+ * Interface for the available operations with server's KV store.
+ */
 public interface KVStoreAdaptor {
 
-    public CompletionObject ver(String key);
+    /**
+     * Return the most recent version of the key in the server.
+     *
+     * @param key
+     * @return
+     */
+    CompletionObject ver(String key);
 
-    public CompletionObject put(String key, byte[] value, long version);
+    /**
+     * Save a value under a key. If it's a new key, then pass 0 as the If-None-Match header, otherwise you must
+     * pass the current version in the database (which may be retrieved with KVStoreAdaptor.get).
+     *
+     * @param key     Existing key or 0 if it's a new key.
+     * @param value   Value to be stored.
+     * @param version Current version in the database.
+     * @return
+     */
+    CompletionObject put(String key, byte[] value, long version);
 
-    public CompletionObject del(String key, long version);
+    /**
+     * Mark a key as deleted in the KV store.
+     *
+     * @param key     The key to save the data under
+     * @param version Version of the key.
+     * @return
+     */
+    CompletionObject del(String key, long version);
 
-    public CompletionObject get(String key, long version);
+    /**
+     * Retrieve a value from a key. If the given version is older than the server's version conflict will be returned.
+     *
+     * @param key     The key to retrieve from the KV store
+     * @param version Current version in the database.
+     * @return
+     */
+    CompletionObject get(String key, long version);
 
-    public CompletionObject keys();
+    /**
+     * Retrieve all the keys from the server belonging to the current user.
+     *
+     * @return
+     */
+    CompletionObject keys();
 
 }
