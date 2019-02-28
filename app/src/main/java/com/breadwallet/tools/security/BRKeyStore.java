842743163e5ce97793d5e129561da7879c14bbc0
xidaokun <15921908346@163.com>
2019-02-28 10:33:26 +0800

    clear clear activity task when authorize exception

--- app/src/main/java/com/breadwallet/tools/security/BRKeyStore.java
+++ app/src/main/java/com/breadwallet/tools/security/BRKeyStore.java
@@ -1,6 +1,7 @@
 package com.breadwallet.tools.security;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.KeyguardManager;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -1062,7 +1063,6 @@ public class BRKeyStore {
                     @Override
                     public void onClick(BRDialogView brDialogView) {
                         BRSharedPrefs.setUserAuthor(app, false);
-
                         brDialogView.dismiss();
                     }
                 }, new BRDialogView.BROnClickListener() {
@@ -1074,6 +1074,8 @@ public class BRKeyStore {
                 }, new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialogInterface) {
+                        ActivityManager activityManager = (ActivityManager) app.getSystemService(Context.ACTIVITY_SERVICE);
+                        activityManager.getAppTasks().clear();
                         android.os.Process.killProcess(android.os.Process.myPid());
                         System.exit(0);
                         System.gc();
