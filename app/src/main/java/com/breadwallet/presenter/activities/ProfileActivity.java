cd74c8fd05bb14ee8208c4c1f5cc557bb1fabc83
xidaokun <15921908346@163.com>
2019-03-07 12:10:03 +0800

    add profile icon

---
+++ app/src/main/java/com/breadwallet/presenter/activities/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.breadwallet.presenter.activities;
+
+import com.breadwallet.presenter.activities.settings.SettingsActivity;
+
+public class ProfileActivity extends SettingsActivity {
+
+    @Override
+    public int getLayoutId() {
+        return super.getLayoutId();
+    }
+
+    @Override
+    public int getBackButtonId() {
+        return super.getBackButtonId();
+    }
+}
