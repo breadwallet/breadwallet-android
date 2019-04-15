a9c6ce5008bbc17290b1d1af1c94dca3329855b2
xidaokun <xidaokun@elastos.org>
2019-04-15 17:53:45 +0800

    授权列表中英文混排序

---
+++ app/src/main/java/com/breadwallet/tools/util/PinyinUtil.java
@@ -0,0 +1,43 @@
+package com.breadwallet.tools.util;
+
+import android.util.Log;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+
+public class PinyinUtil {
+
+
+    public static String getPingYin(String inputString) {
+        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+        format.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        format.setVCharType(HanyuPinyinVCharType.WITH_V);
+
+        char[] input = inputString.trim().toCharArray();
+        String output = "";
+
+        try {
+            for (int i = 0; i < input.length; i++) {
+                if (java.lang.Character.toString(input[i]).matches(
+                        "[\\u4E00-\\u9FA5]+")) {
+                    String[] temp = PinyinHelper.toHanyuPinyinStringArray(
+                            input[i], format);
+                    output += temp[0];
+                }
+                else if (input[i] > 'A' && input[i] < 'Z') {
+                    output += java.lang.Character.toString(input[i]);
+                    output = output.toLowerCase();
+                }
+                output += java.lang.Character.toString(input[i]);
+            }
+        } catch (Exception e) {
+            Log.e("Exception", e.toString());
+        }
+        return output;
+    }
+
+}
