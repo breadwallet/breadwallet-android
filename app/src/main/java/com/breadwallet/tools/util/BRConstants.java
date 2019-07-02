cd826b7f51bbe4292c486b8f46584bc824f56168
Pablo Budelli <pablo.budelli@breadwallet.com>
2019-07-02 17:10:17 +0000

    Cherry-pick DROID-1376 to develop

--- app/src/main/java/com/breadwallet/tools/util/BRConstants.java
+++ app/src/main/java/com/breadwallet/tools/util/BRConstants.java
@@ -215,6 +215,9 @@ public final class BRConstants {
     public static final String CURRENCY = "currency";
     public static final String UTF_8 = "UTF-8";
 
+    // Number of confirmations required to consider a transaction completed.
+    public static final int CONFIRMED_BLOCKS_NUMBER = 6;
+
     /**
      * Commonly used regular expressions.
      */
