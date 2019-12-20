59b648e0ae91ad95ba91fc571566cce93ca54755
Pablo Budelli <pablo.budelli@breadwallet.com>
2019-12-20 20:26:54 +0000

    DROID-1569: Add Fastsync FAQ link

--- app/src/main/java/com/breadwallet/ui/settings/fastsync/FastSyncEffect.kt
+++ app/src/main/java/com/breadwallet/ui/settings/fastsync/FastSyncEffect.kt
@@ -1,20 +1,30 @@
 package com.breadwallet.ui.settings.fastsync
 
 import com.breadwallet.model.SyncMode
+import com.breadwallet.tools.util.BRConstants.FAQ_FASTSYNC
 import com.breadwallet.ui.navigation.NavEffectHolder
 import com.breadwallet.ui.navigation.NavigationEffect
 
 sealed class FastSyncEffect {
     object LoadCurrencyIds : FastSyncEffect()
     object ShowDisableFastSyncDialog : FastSyncEffect()
-    object GoBack : FastSyncEffect(), NavEffectHolder {
-        override val navigationEffect = NavigationEffect.GoBack
+
+    sealed class Nav : FastSyncEffect(), NavEffectHolder {
+        object GoBack : Nav() {
+            override val navigationEffect = NavigationEffect.GoBack
+        }
+
+        object GoToFaq : Nav() {
+            override val navigationEffect = NavigationEffect.GoToFaq(FAQ_FASTSYNC)
+        }
     }
+
     sealed class MetaData : FastSyncEffect() {
         data class SetSyncMode(
             val currencyId: String,
             val mode: SyncMode
         ) : MetaData()
+
         object LoadSyncModes : MetaData()
     }
 }
