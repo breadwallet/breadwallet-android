5f71e3457bd28a71e3f00430703d74e2910c537e
Ahsan Butt <ahsan.butt@breadwallet.com>
2021-02-10 19:08:56 +0000

    Bump to version 4.8.0.3

--- buildSrc/src/main/kotlin/brd/BrdRelease.kt
+++ buildSrc/src/main/kotlin/brd/BrdRelease.kt
@@ -32,7 +32,7 @@ object BrdRelease {
     /** Hot fix version. Usually affected by engineering. Maximum value: 9 */
     private const val engineering = 0
     /** Build version. Increase for each new build. Maximum value: 999 */
-    private const val build = 1
+    private const val build = 3
 
     init {
         check(marketing in 0..99)
